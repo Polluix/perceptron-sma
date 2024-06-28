67b4d1b4be035ec173081d007ddaa2df25a8a071
Polluix <lipe311973@gmail.com>
2024-06-28 17:09:43 -0300

    add: comentários das funções

--- __init__.py
+++ __init__.py
@@ -12,12 +12,17 @@ BIAS = 0.5
 
 def create_database(n:int=2, balanced:bool=True) -> None:
     """
-    Cria base de dados para implementação do perceptron.
+    Cria bases de dados para implementação do perceptron.
     
     --------parâmetros---------
     n: número inteiro de classes desejadas, padronizado em 2.
     balanced: variável booleana. True se as classes devem ser balanceadas,
               False caso contrário.
+
+    O nome de salvamento da base no final deve ser trocado conforme a necessidade,
+    especificando se a base é de treino ou teste.
+    As variaveis x_mean, y_mean alteram o ponto da média dos agrupamentos.
+    rad_x e rad_y alteram o formato da distribuição (gaussiana)
     """
     x_mean = -1
     y_mean = -1
@@ -66,13 +71,28 @@ def create_database(n:int=2, balanced:bool=True) -> None:
 
     return 
 
-def activation_func(v):
+def activation_func(v:float):
+    """
+    função de ativação do perceptron.
+    -----------PARÂMETROS-------------
+    v: (float) resultado do calculo do perceptron (w*x + bias)
+    ----------RETORNO----------------
+    classe do objeto (int).
+    """
     return 1 if v>=BIAS else 0
 
-def perceptron(X, w, bias):
+def perceptron(X, w,bias=BIAS):
+    """
+    Função de utilização do perceptron treinado.
+    --------PARÂMETROS-------------
+    X: (array) vetor de entradas dos objetos que se deseja classificar
+    w: (array) vetor de pesos obtido do treinamento do perceptron
+    -------RETORNO--------------------
+    resultado de classificação de cada objeto presente em X (array).
+    """
     result = []
     for entrada in X:
-        y = activation_func(np.matmul(w,entrada)+bias)
+        y = activation_func(np.matmul(w,entrada)+BIAS)
         result.append(y)
     result = np.array(result)
     return result
@@ -83,21 +103,25 @@ def obtain_folds(base,nfolds):
     ----------PARÂMETROS----------
     base: string com o caminho da base de treino, utilizada na validação. 
     nfolds: número de pastas utilizadas na validação cruzada.
+    -----------RETORNO-----------
+    aux: (array) indices dos elementos de cada classe da base de dados.
     """
     df = pd.read_csv(base)
     df = df.sample(frac=1,random_state=42).reset_index(drop=True)
 
     n = df['CLASSE'].value_counts()
     
-    n_min = n.min()
+    n_min = n.min() #quantidade minima de elementos possiveis para cada classe
     
     assert n_min >= nfolds,f'Uma das classes não possui elementos suficientes para a divisão em {nfolds}'
 
+    # quantidade de objetos de cada classe por pasta
     n_unique_objects_fold = np.array([np.floor(i/n_min) for i in n])
     
     index_list = []
     classes = np.array(df['CLASSE'].unique())
 
+    #indices dos objetos de cada classe dentro da base de dados
     for classe in classes:
         index_list.append(np.where(df['CLASSE']==classe))
     
@@ -122,6 +146,15 @@ def obtain_folds(base,nfolds):
     return aux
 
 def split(skf,nfolds):
+    """
+    Separa a base de dados de treino em pastas para a validação cruzada
+    -------PARÂMETROS--------
+    skf: (array) indices dos elementos de cada classe na base de dados.
+    nfolds: (int) numero de pastas para a validação cruzada.
+    ----------RETORNO--------
+    train_indexes: (array) indices na base de dados dos elementos de treino de treino para cada etapa da validação cruzada.
+    test_indexes: (array) indices na base de dados dos elementos de teste para cada etapa da validação cruzada.
+    """
     train_indexes = []
     test_indexes = []
     for i in range(nfolds):
@@ -144,6 +177,10 @@ def cross_validation(base:str, nfolds=10):
     Numero de dobras padronizado em 10.
     ------PARÂMETROS------
     base: (string) caminho para a base primária para gerar as bases da validação cruzada.
+    nfolds: (int) número de pastas para a divisão da base de dados para a validaçao cruzada.
+    -----RETORNO----------
+    MSE: (float array) erros médios quadráticos de cada etapa de validação
+    std: (float array) desvios padrão de cada dos erros quadráticos de cada etapa de validação.
     """
 
     df = pd.read_csv(base, sep=',', decimal='.')
@@ -189,6 +226,9 @@ def train_model(X:np.array, Y:np.array, bias:float=0, learn_rate:float=0.01,nepo
     Y: (array dimensão n) contém as classes de cada objeto.
     learn_rate: (float) taxa de aprendizado do perceptron. Valor padrão 0.01
     bias: (float) bias do perceptron. Valor padrão 0
+    -------RETORNO----------
+    w: (array) vetor de pesos ajustado após o treino do modelo.
+    erro_med: (float) erros quadráticos médios a cada época de treinamento.
     """
     lin,col = np.shape(X)
     w = np.full(col, 0)
@@ -207,11 +247,30 @@ def train_model(X:np.array, Y:np.array, bias:float=0, learn_rate:float=0.01,nepo
 
     return w, erro_med
 
-def fronteira_decisao(x, weight, bias):
+def fronteira_decisao(x, weight, bias=BIAS):
+        """
+        Calcula a fronteira de decisão de um perceptron treinado para um conjunto de pontos.
+        Função aplicável para problemas de classificação binária em 2D.
+        ---------PARÂMETROS------------
+        x: (array) pontos nos quais se deseja calcular a fronteira de decisão.
+        weight: (array) vetor de pesos do perceptron treinado.
+        bias: (float, optional) bias utilizado durante o treinamento.
+        -----------RETORNO-------------
+        valores (x,y) de cada ponto da fronteira de decisão do perceptron treinado
+        no espaço 2D.
+        """
         return ((-weight[0]/weight[1])*x+(bias)/weight[1])
 
 # _____________________TREINO_________________________
 def treina_modelo():
+    """
+    Implementa lógica de treinamento em 2D
+    -------PARÂMETROS-------
+    None.
+    ---------RETORNO--------
+    w: (float array) vetor de pesos ajustado no treinamento.
+    e: (float array) erros quadráticos médios a cada época de treinamento.
+    """
 
     MSEs,stds = cross_validation(base_treino,nfolds=7)
 
@@ -220,6 +279,8 @@ def treina_modelo():
 
     #plot da função de resultado de treino
     df = pd.read_csv(base_treino)
+
+    #randomiza a base de dados. Pode ser usada para testar a influencia no treino.
     # df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 
     X = np.array(df.drop(columns=['CLASSE'])) #variáveis do modelo
@@ -233,6 +294,15 @@ def treina_modelo():
 # __________________________TESTE___________________________
 
 def testa_modelo(w):
+    """
+    Implementa lógica de teste do modelo treinado em 2D.
+    ---------PARÂMETROS---------
+    w: (float array) vetor de pesos ajustados obtidos do treinamento.
+    -----------RETORNO-----------
+    X_teste: (array) conjunto de pontos utilizado no teste.
+    d_teste: (array) classes de cada objeto presente em X_teste.
+    resultados: (array) resultados de classificação de cada elemento de X_teste pela rede perceptron.
+    """
     
 
     df_teste = pd.read_csv(base_teste)
@@ -248,6 +318,12 @@ def testa_modelo(w):
 
 # ____________________AVALIAÇÃO____________________________
 def avalia_modelo( d_teste, resultados):
+    """
+    Obtém o número de classificações corretas do modelo treinado e testado.
+    ----------PARÂMETROS------------
+    d_teste: (array) classes de cada elemento presente na base de dados de teste.
+    resultados: (array) resultados de classificação de cada elemento de teste pela rede perceptron.
+    """
     soma = 0
     for i in range(len(d_teste)):
         if resultados[i] == d_teste[i]:
@@ -258,6 +334,12 @@ def avalia_modelo( d_teste, resultados):
 
 # _______________________CHECAGEM BASES______________________
 def valida_bases():
+    """
+    Verifica se as bases de dados de treino e teste nao possuem elementos em comum.
+    Utilizada para garantir que haja generalização da rede perceptron após
+    o treinamento.
+    """
+
     df1 = pd.read_csv(base_treino)
     df2 = pd.read_csv(base_teste)
 
@@ -272,6 +354,16 @@ def valida_bases():
         print("Os DataFrames possuem elementos em comum.")
 
 def plot_fronteira_decisao(X, d, weight, bias,verbose=False):
+    """
+    Implementa lógica de plotagem da fronteira de decisão de 
+    uma rede perceptron treinada em um problema de 2D.
+    ---------PARÂMETROS-------------
+    X: (array) entradas da rede perceptron.
+    d: (array) classes de cada elemento de X.
+    weight: (array) vetor de pesos ajustados.
+    verbose: (bool, optional) utilizado para salvar, se True,
+            ou não salvar a figura plotada, se False.
+    """
     n =len(d)
 
     classe1, classe2 = [], []
@@ -301,20 +393,23 @@ def plot_fronteira_decisao(X, d, weight, bias,verbose=False):
     plt.show()
 
     
-
+#cria bases de dados
 # create_database(2,False)
 
+#verifica se bases de treino e teste não possuem elementos em comum
 valida_bases()
 
+#treina modelo e obtem vetor de pesos ajustado e erros médios quadráticos
+w, erro_med = treina_modelo()#
 
-w, erro_med = treina_modelo()#vetor de pesos obtido do treino do modelo
 
-
-
+#testa o modelo treinado
 X, d1, resultados = testa_modelo(w)
 
+#obtem o numero de classificações corretas
 avalia_modelo(d1,resultados)
 
+#plota fronteira de decisão 2D
 plot_fronteira_decisao(X,d1,w,BIAS,True)
 
 # ----------------------PLOTS RELATORIO-----------------------
@@ -389,7 +484,7 @@ plt.savefig('Fonteiras_treinamento')
 plt.show()
 # ----------------------------------------------------------------------------------------------
 
-# ------------------------------------matriz de confusão-------------------------------------------
+# ------------------------------------MATRIZ DE CONFUSÃO-------------------------------------------
 # as bases para treino e teste devem ser alteradas nas variáveis base_treino e base_teste, nas linhas 8 e 9
 VP = 0
 VN = 0
